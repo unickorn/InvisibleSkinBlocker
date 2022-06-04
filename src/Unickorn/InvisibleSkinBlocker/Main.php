a498aa35e3b29cf285a25bc22ea5e41660df2424
Nick <nick@npbe.net>
2022-06-04 09:13:31 +0300

    ✨ configurable behaviour: "block"

--- src/Unickorn/InvisibleSkinBlocker/Main.php
+++ src/Unickorn/InvisibleSkinBlocker/Main.php
@@ -3,11 +3,13 @@ declare(strict_types=1);
 
 namespace Unickorn\InvisibleSkinBlocker;
 
+use pocketmine\entity\Skin;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerChangeSkinEvent;
 use pocketmine\event\player\PlayerLoginEvent;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
+use Ramsey\Uuid\Uuid;
 use function ord;
 use function strlen;
 
@@ -26,11 +28,18 @@ class Main extends PluginBase implements Listener
 
 	private int $percentage;
 	private string $message;
+	private string $behaviour;
+	private ?string $defaultSkin = null;
 
 	public function onEnable() : void {
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->percentage = $this->getConfig()->get("percentage", 75);
-		$this->message = $this->getConfig()->get("message", "Invisible skins are not allowed.");
+		$c = $this->getConfig();
+		$this->percentage = $c->get("percentage", 75);
+		$this->message = $c->get("message", "Invisible skins are not allowed.");
+		$this->behaviour = $c->get("behaviour", "kick");
+		if ($defaultSkin = $c->get("default-skin")) {
+			$this->defaultSkin = $this->loadSkin($defaultSkin["geometry"], $defaultSkin["texture"]);
+		}
 	}
 
 	public function checkSkin(Player $player, ?string $skinData = null) : bool {
@@ -52,17 +61,66 @@ class Main extends PluginBase implements Listener
 		return true;
 	}
 
+	private function loadSkin(string $geometry, string $texture) : ?Skin {
+		$geometryData = "";
+		if ($geometry !== "") {
+			try {
+				$customGeometry = json_decode(file_get_contents($this->getDataFolder() . $geometry), true, 512, JSON_THROW_ON_ERROR);
+			} catch (\Exception $e) {
+				$this->getLogger()->error("Could not load custom geometry file, reverting back to random bytes: " . $e->getMessage());
+				return null;
+			}
+			$geometry = $customGeometry["minecraft:geometry"][0]["description"]["identifier"];
+		}
+
+		if ($texture !== "") {
+			$f = $this->getDataFolder() . $texture;
+			$image = imagecreatefrompng($f);
+			[$width, $height] = getimagesize($f);
+			$bytes = '';
+			for ($y = 0; $y < $height; ++$y) {
+				for ($x = 0; $x < $width; ++$x) {
+					$color = @imagecolorsforindex($image, @imagecolorat($image, $x, $y));
+					$bytes .= chr($color['red']) . chr($color['green']) . chr($color['blue']) . chr((($color['alpha'] << 1) ^ 0xff) - 1);
+				}
+			}
+			imagedestroy($image);
+			return new Skin(Uuid::uuid4()->toString(), $bytes, "", $geometry, $geometryData);
+		}
+
+		return null;
+	}
+
+	public function randomSkin() : ?Skin {
+		$bytes = '';
+		for ($i = 0; $i < 2048; $i++) {
+			$bytes .= chr(mt_rand(0, 255)) . chr(mt_rand(0, 255)) . chr(mt_rand(0, 255)) . chr(255);
+		}
+		return new Skin(Uuid::uuid4()->toString(), $bytes, "", "geometry.humanoid.custom", "");
+	}
+
 	public function onJoin(PlayerLoginEvent $event) : void {
-		if($this->checkSkin($event->getPlayer())){
-			$event->setKickMessage($this->message);
-			$event->cancel();
+		$player = $event->getPlayer();
+		if ($this->checkSkin($player)) {
+			if ($this->behaviour === "kick") {
+				$event->cancel();
+				$player->kick($this->message);
+			} else {
+				$player->setSkin($this->defaultSkin ?? $this->randomSkin());
+			}
 		}
 	}
 
 	public function onChangeSkin(PlayerChangeSkinEvent $event) : void {
 		$player = $event->getPlayer();
-		if($this->checkSkin($player, $event->getNewSkin()->getSkinData())){
-			$player->kick($this->message);
+		if ($this->checkSkin($player, $event->getNewSkin()->getSkinData())) {
+			if ($this->behaviour === "kick") {
+				$event->cancel();
+				$player->kick($this->message);
+			} else {
+				$player->setSkin($this->defaultSkin ?? $this->randomSkin());
+				$player->sendMessage($this->message);
+			}
 		}
 	}
 }
